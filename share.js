04fe44911ddcb2e6ce4cfaa69d01816ca758df8e
aagamjain-git <aagamjain99@gmail.com>
2020-02-26 00:56:23 +0530

    added toggleButton

--- share.js
+++ share.js
@@ -1,6 +1,7 @@
 var backdrop=document.querySelector(".backdrop");
  var modal=document.querySelector(".modal");
  var plan_sec_button=document.querySelectorAll("#plan-sec button");
+ var noButton=document.querySelector("#no");
  for(var i=0;i<plan_sec_button.length;i++)
  {
      plan_sec_button[i].addEventListener("click",function(){ 
@@ -9,3 +10,19 @@ var backdrop=document.querySelector(".backdrop");
 
      });
  }
+ var mobilenav=document.querySelector(".mobile-nav");
+ var toggle=document.querySelector(".toggle-button");
+ toggle.addEventListener("click",function(){
+     mobilenav.style.display= "block";
+     backdrop.style.display="block";
+ })
+
+ backdrop.addEventListener("click",function(){
+    mobilenav.style.display= "none";
+    backdrop.style.display="none";
+    modal.style.display = "none";
+ })
+ noButton.addEventListener("click",function(){
+    backdrop.style.display="none";
+    modal.style.display = "none";
+ })
